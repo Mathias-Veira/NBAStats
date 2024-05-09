602d20e80e474c2de600c093ff5e166dfa3da67e
Mathias-Veira <mveirad@gmail.com>
2024-05-09 20:23:08 +0200

    Update rankings

--- nba_stats/lib/view/standings.dart
+++ nba_stats/lib/view/standings.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:nba_stats/controller/api_request.dart';
 import 'package:nba_stats/model/equipo.dart';
 
@@ -24,27 +25,55 @@ class _standingsState extends State<standings> {
     // Espera a que se complete el Future y obtén el resultado
     List<Equipo>? ranking = await ApiService.getRanking();
 
-    // Verifica si el resultado no es nulo
-    if (ranking != null) {
-      // Actualiza el estado con el resultado
-      setState(() {
-        equipos = ranking;
-      });
-    } else {
-      // Maneja el caso en el que no se pueda obtener el ranking
-      // Puedes mostrar un mensaje de error o realizar otras acciones según sea necesario
-    }
+    // Actualiza el estado con el resultado
+    setState(() {
+      equipos = ranking;
+    });
   }
+
   @override
   Widget build(BuildContext context) {
+    List<String> listIDs = List<String>.generate(31, (counter) => "$counter");
+    int i = 1;
     return SingleChildScrollView(
-      child: DataTable(
-        columns: [
-          DataColumn(label: Text("Name"))
-        ], 
-        rows: equipos.map((equipo) => DataRow(cells:[
-          DataCell(Text(equipo.nombreEquipo)),
-        ])).toList()),
+      scrollDirection: Axis.vertical,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: DataTable(
+          columnSpacing: 15.5,
+            columns: [
+              DataColumn(label: Text("Ranking")),
+              DataColumn(label: Text("")),
+              DataColumn(label: Text("Name")),
+              DataColumn(label: Text("W")),
+              DataColumn(label: Text("L")),
+              DataColumn(label: Text("PCT")),
+              DataColumn(label: Text("Conference")),
+              DataColumn(label: Text("Division")),
+            ],
+            rows: equipos
+                .map((equipo) => DataRow(cells: [
+                      DataCell(Text(listIDs[i++])),
+                      DataCell(
+                        Container(
+                            width: 40,
+                            decoration: const BoxDecoration(shape: BoxShape.circle),
+                            child: Image.asset('assets/img_teams/${equipo.ciudadEquipo} ${equipo.nombreEquipo}.png'),
+                          ),
+                      ),
+                      DataCell(Text(equipo.abreviacionEquipo)),
+                      DataCell(Text(equipo.nVictorias.toString())),
+                      DataCell(Text(equipo.nDerrotas.toString())),
+                      DataCell(Text(equipo.porcentajeVictorias.toString())),
+                      DataCell(Text(equipo.conferenciaEquipo)),
+                      DataCell(Text(equipo.divisionEquipo)),
+      
+                    ]))
+                .toList()),
+      ),
     );
   }
 }
+
+
+
