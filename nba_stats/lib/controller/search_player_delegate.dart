f04a277ef99319e05a866cec0d1ffef5ab0f5f49
Mathias-Veira <mveirad@gmail.com>
2024-05-11 12:58:57 +0200

    añadir barra de búsqueda para los jugadores

---
+++ nba_stats/lib/controller/search_player_delegate.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:nba_stats/controller/api_request.dart';
+
+import '../model/player.dart';
+
+class SearchPlayerDelegate extends SearchDelegate {
+  List<Jugador> players;
+  List<Jugador> _filter = [];
+  SearchPlayerDelegate(this.players);
+
+  void getPlayers() async {
+    players = await ApiService.listarJugadores();
+  }
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: () {
+            query = '';
+          },
+          icon: Icon(Icons.close))
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        icon: Icon(Icons.arrow_back));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Center();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    getPlayers();
+    _filter = players.where((element) {
+      return element.nombreJugador
+              .toLowerCase()
+              .contains(query.trim().toLowerCase()) ||
+          element.apellidoJugador
+              .toLowerCase()
+              .contains(query.trim().toLowerCase());
+    }).toList();
+    return ListView.builder(
+        itemCount: _filter.length,
+        itemBuilder: (_, index) {
+          return Card(
+            child: ListTile(
+            
+              title: Text(_filter[index].nombreJugador +
+                  ' ' +
+                  _filter[index].apellidoJugador),
+            ),
+          );
+        });
+  }
+}
--- nba_stats/lib/view/players.dart
+++ nba_stats/lib/view/players.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:nba_stats/controller/search_player_delegate.dart';
+
+import '../model/player.dart';
 
 class Players extends StatefulWidget {
   const Players({super.key});
@@ -8,14 +11,23 @@ class Players extends StatefulWidget {
 }
 
 class _PlayersState extends State<Players> {
+  List<Jugador> players = [];
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(
-          child: Text("Players Works!"),
-        )
-      ]
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: ListTile(
+          trailing: Icon(Icons.search),
+          onTap: () {
+            showSearch(
+                context: context, delegate: SearchPlayerDelegate(players));
+          },
+          title: const Text(
+            'Search Players',
+          ),
+        ),
+      ),
     );
   }
 }
